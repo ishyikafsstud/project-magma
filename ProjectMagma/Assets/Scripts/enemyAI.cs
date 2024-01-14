2217818990b1e8ffeb8555c25a5c3f2db89466aa
Sccchase <SCChase@student.fullsail.edu>
2024-01-14 18:49:25 -0500

    UI prefab, updated enemy nav

--- ProjectMagma/Assets/Scripts/enemyAI.cs
+++ ProjectMagma/Assets/Scripts/enemyAI.cs
@@ -23,18 +23,27 @@ public class enemyAI : MonoBehaviour, IDamage
     void Update()
     {
         //this will not work till the GameManager is in the scene. in class we did it within the UI
-        //transform.position = Vector3.MoveTowards(transform.position, gameManager.instance.player.transform.position, speed * Time.deltaTime);
-        //agent.SetDestination(gameManager.instance.player.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, gameManager.instance.player.transform.position, speed * Time.deltaTime);
+        agent.SetDestination(gameManager.instance.player.transform.position);
         
     }
 
     public void takeDamage(int amount)
     {
         HP -= amount;
+        StartCoroutine(flashRed());
         
         if(HP <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+
     }
 }
--- ProjectMagma/Assets/Scripts/gameManager.cs
+++ ProjectMagma/Assets/Scripts/gameManager.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    //this may not work till the GameManager is in the scene. in class we did it through the ui
+    //if you need to test->i added a UI prefab with a GameManager to add to your scene
+    public static gameManager instance;
+
+    public GameObject player;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-      
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
