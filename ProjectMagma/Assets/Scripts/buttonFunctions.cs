4cd1d276fe8dec567b48e9a8326c93b1b9d518d3
Ethan Brunswick <EDBrunswick@student.fullsail.edu>
2024-01-15 16:38:44 -0600

    Implemented Button Functions
    Implemented Pause, Unpausing, Restart and Quit button functionality within the menus, buttonFunctions Script and the gameManager Script. All updates have been saved to the UI-GameManager prefab.

---
+++ ProjectMagma/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class buttonFunctions : MonoBehaviour
+{
+  
+    public void Resume()
+    {
+        // Call Unpause within game manager
+        gameManager.instance.stateUnpaused();
+    }
+
+    
+    public void Restart() // Will need to find another more efficient way to implement this
+    {
+        // Loads entire scene the user is currently in
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // Reset Time scale
+        gameManager.instance.stateUnpaused();
+    }
+
+    public void Quit()
+    {
+        // Close application
+        Application.Quit();
+    }
+}
--- ProjectMagma/Assets/Scripts/gameManager.cs
+++ ProjectMagma/Assets/Scripts/gameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class gameManager : MonoBehaviour
@@ -8,8 +9,13 @@ public class gameManager : MonoBehaviour
     //if you need to test, use the UI prefab with a GameManager in it for your scene
     public static gameManager instance;
 
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+
     public GameObject player;
-    
+
+    public bool isPaused;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -20,6 +26,42 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // If ESC button is pressed and nothing is currently in the active menu
+        if (Input.GetButtonDown("Cancel") && menuActive == null)
+        {
+            // call pause function
+            statePaused();
+            // set pause menu as the active menu
+            menuActive = menuPause;
+            // toggel the menu on
+            menuActive.SetActive(isPaused);
+        }
+    }
+    public void statePaused()
+    {
+        // Toggel the isPaused Bool
+        isPaused = !isPaused;
+        // Stop all time based Actions from happening in the background
+        Time.timeScale = 0;
+        // Make Cursor Visible
+        Cursor.visible = true;
+        // Confine Cursor to Pause window boundaries
+        Cursor.lockState = CursorLockMode.Confined;
+
+    }
+    public void stateUnpaused()
+    {
+        // Toggel the isPaused Bool
+        isPaused = !isPaused;
+        // Resumes time based actions 
+        Time.timeScale = 1;
+        // Hides Cursor
+        Cursor.visible = false;
+        // Locks Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        // Toggle Menu Off
+        menuActive.SetActive(false);
+        // resets and removes pause menu from active Menu
+        menuActive = null;
     }
 }
