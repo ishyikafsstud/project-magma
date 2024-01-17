3d42f100589e656075f9cd77a85da80841e499c4
Ivan Shyika <141061813+ishyikafsstud@users.noreply.github.com>
2024-01-17 16:59:58 -0500

    Fix the AABB error

--- ProjectMagma/Assets/Scripts/playerController.cs
+++ ProjectMagma/Assets/Scripts/playerController.cs
@@ -25,16 +25,11 @@ public class playerController : MonoBehaviour, IDamage
     [SerializeField] int shootDamage;
     [SerializeField] float shootRate;
     [SerializeField] int shootDist;
-    [SerializeField] float energyCost;
-    [SerializeField] int energy;
-    [SerializeField] float regenRate;
-    [SerializeField] float energyFillSpeed;
 
     [Header("UI")]
     [Tooltip("The duration of screen flash upon receiving damage.")]
     [SerializeField] float damageFlashDuration;
 
-    private float currentEnergy;
     private int energyOriganal;
     private int healthOriginal;
     private Vector3 horMotionDirection;
@@ -49,7 +44,6 @@ public class playerController : MonoBehaviour, IDamage
     void Start()
     {
         healthOriginal = health;
-        energyOriganal = energy;
         currentSpeed = walkSpeed;
 
         updatePlayerUI();
@@ -62,14 +56,9 @@ public class playerController : MonoBehaviour, IDamage
         processMovement();
         Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.green);
 
-        if (Input.GetButton("Shoot") && !isShooting && currentEnergy > 0)
+        if (Input.GetButton("Shoot") && !isShooting)
         {
             StartCoroutine(Shoot());
-            useEnergy();
-        }
-        else
-        {
-            RegenEnergy();
         }
         
     }
@@ -160,32 +149,6 @@ public class playerController : MonoBehaviour, IDamage
 
         isShooting = false;
     }
-
-    void useEnergy()
-    {
-        if (isShooting && currentEnergy > 0)
-        {
-            currentEnergy -= energyCost;
-            if (currentEnergy < 0)
-            {
-                currentEnergy = 0;
-                isShooting = false;
-            }
-            updatePlayerUI();
-        }
-    }
-
-    void RegenEnergy()
-    {
-        if (currentSpeed > 0 && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") !=0))
-        {
-            currentEnergy += regenRate * Time.deltaTime;
-            currentEnergy = Mathf.Clamp(currentEnergy, 0, energyOriganal);
-            
-        }
-        updatePlayerUI();
-
-    }
     void die()
     {
         gameManager.instance.scenarioPlayerLoses();
@@ -194,7 +157,6 @@ public class playerController : MonoBehaviour, IDamage
     public void respawn()
     {
         health = healthOriginal;
-        energy = energyOriganal;
         updatePlayerUI();
 
         controller.enabled = false;
@@ -205,11 +167,7 @@ public class playerController : MonoBehaviour, IDamage
 
     void updatePlayerUI()
     {
-        //health bar update
         gameManager.instance.playerHealthbar.fillAmount = (float)health / healthOriginal;
-        //energy bar update
-        float energyFillAmount = Mathf.Lerp(gameManager.instance.playerEnergybar.fillAmount, (float)currentEnergy / energyOriganal, Time.deltaTime * energyFillSpeed);
-        gameManager.instance.playerEnergybar.fillAmount = energyFillAmount;
     }
 
     IEnumerator flashDamageOnScreen()
