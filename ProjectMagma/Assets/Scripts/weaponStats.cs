26400168ffc416b1c3c89c5636fc6af1ea1ae4ff
Ivan Shyika <141061813+ishyikafsstud@users.noreply.github.com>
2024-02-02 19:28:57 -0500

    Update weaponStats.cs

--- ProjectMagma/Assets/Scripts/weaponStats.cs
+++ ProjectMagma/Assets/Scripts/weaponStats.cs
@@ -10,9 +10,19 @@ public class weaponStats : ScriptableObject
     public float shootRate;
     public int shootDist;
     public float energyCostPerShot;
+    
+    public WeaponTypes weaponType;
+    [Tooltip("Prefab of the projectile shot by this weapon. Ignore for raycast-based weapons.")]
+    public GameObject projectilePrefab;
 
     public GameObject model;
     public ParticleSystem hitEffect;
     public AudioClip shootSound;
     [Range(0, 1)] public float shootSoundVolume;
+
+    public enum WeaponTypes
+    {
+        Raycast,
+        Projectile,
+    }
 }
