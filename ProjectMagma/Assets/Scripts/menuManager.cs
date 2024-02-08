15a8377c54622481ea143483e7e69a3087386cfd
Ethan Brunswick <EDBrunswick@student.fullsail.edu>
2024-02-07 21:46:51 -0600

    Updated Main Menu, Pause Menu, and menuManager
    Main Menu now has a level select option.
    Pause menu was re organized - buttons moved to left side
    menuManger was cleaned up - removed unnecessary code

--- ProjectMagma/Assets/Scripts/menuManager.cs
+++ ProjectMagma/Assets/Scripts/menuManager.cs
@@ -8,13 +8,91 @@ public class menuManager : MonoBehaviour
 {
     public static menuManager instance;
 
-    [Header("- Load Scene -")]
-    [SerializeField] string levelOne;
+    [Header("---- Load Scene ----")]
     [SerializeField] string mainMenu;
+    [SerializeField] string levelOne;
+    [SerializeField] string levelTwo;
+    [SerializeField] string levelThree;
+    [SerializeField] string levelFour;
+    [SerializeField] string levelFive;
 
-    [Header("- Sub Windows -")]
-    [SerializeField] GameObject ControlsWindow;
-    [SerializeField] GameObject CreditsWindow;
+    public void MainMenu()
+    {
+        if (mainMenu != null)
+        {
+            SceneManager.LoadScene(mainMenu);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Debug.LogWarning("Scene is not set in inspector.");
+        }
+    }
+    public void LoadLevelOne()
+    {
+        if (levelOne != null)
+        {
+            SceneManager.LoadScene(levelOne);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Debug.LogWarning("Scene is not set in inspector.");
+        }
+    }
+    public void LoadLevelTwo()
+    {
+        if (levelTwo != null)
+        {
+            SceneManager.LoadScene(levelTwo);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Debug.LogWarning("Scene is not set in inspector.");
+        }
+    }
+    public void LoadLevelThree()
+    {
+        if (levelThree != null)
+        {
+            SceneManager.LoadScene(levelThree);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Debug.LogWarning("Scene is not set in inspector.");
+        }
+    }
+    public void LoadLevelFour()
+    {
+        if (levelFour != null)
+        {
+            SceneManager.LoadScene(levelFour);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Debug.LogWarning("Scene is not set in inspector.");
+        }
+    }
+    public void LoadLevelFive()
+    {
+        if (levelFive != null)
+        {
+            SceneManager.LoadScene(levelFive);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Debug.LogWarning("Scene is not set in inspector.");
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
     public void ToggleWindow(GameObject window)
     {
@@ -37,43 +115,5 @@ public class menuManager : MonoBehaviour
         {
             Debug.LogWarning("Reference not set in inspector.");
         }
-    }
-    public void PlayGame()
-    {
-        if (levelOne != null)
-        {
-            SceneManager.LoadScene(levelOne);
-            Time.timeScale = 1.0f;
-        }
-        else
-        {
-            Debug.LogWarning("Scene is not set in inspector.");
-        }
-    }
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
-    public void MainMenu()
-    {
-        if (mainMenu != null)
-        {
-            SceneManager.LoadScene(mainMenu);
-            Time.timeScale = 1.0f;
-        }
-        else
-        {
-            Debug.LogWarning("Scene is not set in inspector.");
-        }
-    }
-
-    public void Controls()
-    {
-        ToggleWindow(ControlsWindow);
-    }
-    public void Credits()
-    {
-        ToggleWindow(CreditsWindow);
     }
 }
