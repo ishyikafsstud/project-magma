3028cfccc3a3cebcc26e795ef1d5551009b013b3
Ivan Shyika <141061813+ishyikafsstud@users.noreply.github.com>
2024-02-12 15:27:47 -0500

    Create Spiderling and BroodMother enemies

--- ProjectMagma/Assets/Scripts/Utility/spawnObjectsOnDeath.cs
+++ ProjectMagma/Assets/Scripts/Utility/spawnObjectsOnDeath.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class spawnObjectsOnDeath : MonoBehaviour
 {
 
-    [Tooltip("The object to spawn.")]
-    [SerializeField] GameObject spawnObject;
+    [Tooltip("The enemy to spawn.")]
+    [SerializeField] GameObject spawnedEnemyPrefab;
     [Tooltip("The spawn positions for the object. The specified object will be spawned at each of them.")]
     [SerializeField] List<GameObject> spawnPositions = new List<GameObject>();
 
-    enemyAI parent;
+    enemyAI enemyAIComponent;
 
     // Start is called before the first frame update
     void Start()
     {
-        parent = GetComponent<enemyAI>();
-        parent.OnDied += SpawnObjectsOnDeath;
+        enemyAIComponent = GetComponent<enemyAI>();
+        enemyAIComponent.OnDied += SpawnObjectsOnDeath;
     }
 
     void SpawnObjectsOnDeath(GameObject diedEnemy)
     {
         foreach (GameObject spawnPos in spawnPositions)
         {
-            Instantiate(spawnObject, spawnPos.transform.position, spawnPos.transform.rotation);
+            GameObject enemyInst = Instantiate(spawnedEnemyPrefab, spawnPos.transform.position, spawnPos.transform.rotation);
+            enemyAI spawnedEnemyScript = enemyInst.GetComponent<enemyAI>();
+            spawnedEnemyScript.IsMinion = true;
+            spawnedEnemyScript.BecomeAlerted(gameManager.instance.player.transform.position);
         }
     }
 }
