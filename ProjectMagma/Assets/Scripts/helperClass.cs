5cbbc2a5bd2be93f216493c5547f1d6fc6c2a51d
Ivan Shyika <141061813+ishyikafsstud@users.noreply.github.com>
2024-02-15 21:09:52 -0500

    Create weapon items list in helperClass

--- ProjectMagma/Assets/Scripts/helperClass.cs
+++ ProjectMagma/Assets/Scripts/helperClass.cs
@@ -10,4 +10,11 @@ public class helperClass : MonoBehaviour
         "with the int value of their wandType. (e.g. Electric has the value of 0 so it must be the first in the list).\n" +
         "TL;DR - UNLESS YOU ARE IVAN, DON'T MESS WITH THIS LIST.")]
     [SerializeField] public List<weaponStats> weaponList;
+
+    // TODO: this should be a serialized dictionary. Unity does not have built-in serialized
+    // dictionaries, so we would need to write our own.
+    [Tooltip("Make sure the position of weapon items in the list corresponds " +
+        "with the int value of their wandType. (e.g. Electric has the value of 0 so it must be the first in the list).\n" +
+        "TL;DR - UNLESS YOU ARE IVAN, DON'T MESS WITH THIS LIST.")]
+    [SerializeField] public List<GameObject> weaponItemsList;
 }
