c70278a664e7aac5d03fa69224952713ffa5b729
Ivan Shyika <141061813+ishyikafsstud@users.noreply.github.com>
2024-02-16 12:53:12 -0500

    Update weaponPickup.cs

--- ProjectMagma/Assets/Scripts/weaponPickup.cs
+++ ProjectMagma/Assets/Scripts/weaponPickup.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class weaponPickup : MonoBehaviour
+public class weaponPickup : pickableItemBase
 {
     [SerializeField] weaponStats weapon;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
+    public override void Pickup()
     {
         gameManager.instance.playerScript.pickupWeapon(weapon);
         Destroy(gameObject);
