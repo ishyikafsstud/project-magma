6166c4f510add18afb7e88e948157ed09a13c89f
snazbear <tsbrock@student.fullsail.edu>
2024-02-28 13:46:01 -0600

    Fixed healingCircle bug where player only heals on entry or exit

--- ProjectMagma/Assets/Scripts/healingCircle.cs
+++ ProjectMagma/Assets/Scripts/healingCircle.cs
@@ -6,17 +6,18 @@ public class healingCircle : MonoBehaviour
 {
     [SerializeField] int healingRate;// Healing rate per second
     public playerController playerController;
+    private bool isHealing = false;
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if(other.CompareTag("Player"))
+        if (isHealing)
         {
             if (gameManager.instance.player != null)
             {
-                
+
                 if (playerController == null)
                 {
-                    playerController = gameManager.instance.player.GetComponent<playerController>();
+                    playerController = gameManager.instance.playerScript;
                 }
 
                 // Heal the player over time
@@ -25,23 +26,13 @@ public class healingCircle : MonoBehaviour
             }
         }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        isHealing = true;
+    }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            if (gameManager.instance.player != null)
-            {
-
-                if (playerController == null)
-                {
-                    playerController = gameManager.instance.player.GetComponent<playerController>();
-                }
-
-                // Heal the player over time
-                int healingAmount = Mathf.RoundToInt(healingRate * Time.deltaTime);
-                playerController.Heal(healingAmount);
-            }
-        }
+        isHealing = false;
     }
 }
